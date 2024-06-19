834203e42fa8cef5e3bd7bf3c5ec63cc8a0995ea
Warushika-Wijayarathna <warushikaWijayarathna@gmail.com>
2024-06-19 18:35:17 +0530

    Button Actions

--- Cashier-SC/src/main/java/controller/DashboardFormController.java
+++ Cashier-SC/src/main/java/controller/DashboardFormController.java
@@ -27,21 +27,83 @@ public class DashboardFormController {
     }
 
     public void popcornBtnOnAction(ActionEvent actionEvent) {
-        int qty = 0;
-//        ItemTm itemTm = new ItemTm("Popcorn",qty+1, 200.00);
-//        tblCart.getItems().add(itemTm);
+        boolean isPopCornExists = false;
+        for (Object item : tblCart.getItems()) {
+            ItemTm itemTm = (ItemTm) item;
+            if (itemTm.getDescription().equals("Popcorn")){
+                itemTm.setQty(itemTm.getQty()+1);
+                itemTm.setPrice(itemTm.getPrice() + 200);
+                isPopCornExists = true;
+                break;
+            }
+        }
 
-        // if there is popcorn is inside the cart already
-        if
+        if (!isPopCornExists) {
+            ItemTm itemTm = new ItemTm("Popcorn", 1, 200.00);
+            tblCart.getItems().add(itemTm);
+        }
+
+        tblCart.refresh();
     }
 
     public void chipsBtnOnAction(ActionEvent actionEvent) {
+        boolean isChipsExists = false;
+        for (Object item : tblCart.getItems()) {
+            ItemTm itemTm = (ItemTm) item;
+            if (itemTm.getDescription().equals("Chips")){
+                itemTm.setQty(itemTm.getQty()+1);
+                itemTm.setPrice(itemTm.getPrice() + 200);
+                isChipsExists = true;
+                break;
+            }
+        }
+
+        if (!isChipsExists) {
+            ItemTm itemTm = new ItemTm("Chips", 1, 100.00);
+            tblCart.getItems().add(itemTm);
+        }
+
+        tblCart.refresh();
     }
 
     public void colaOnAction(ActionEvent actionEvent) {
+        boolean isCocacolaExists = false;
+        for (Object item : tblCart.getItems()) {
+            ItemTm itemTm = (ItemTm) item;
+            if (itemTm.getDescription().equals("Cocacola")){
+                itemTm.setQty(itemTm.getQty()+1);
+                itemTm.setPrice(itemTm.getPrice() + 300);
+                isCocacolaExists = true;
+                break;
+            }
+        }
+
+        if (!isCocacolaExists) {
+            ItemTm itemTm = new ItemTm("Cocacola", 1, 300.00);
+            tblCart.getItems().add(itemTm);
+        }
+
+        tblCart.refresh();
     }
 
     public void spriteOnAction(ActionEvent actionEvent) {
+        boolean isSpiteExists = false;
+        for (Object item : tblCart.getItems()) {
+            ItemTm itemTm = (ItemTm) item;
+            if (itemTm.getDescription().equals("Sprite")){
+                itemTm.setQty(itemTm.getQty()+1);
+                itemTm.setPrice(itemTm.getPrice() + 300);
+                isSpiteExists = true;
+                break;
+            }
+        }
+
+        if (!isSpiteExists) {
+            ItemTm itemTm = new ItemTm("Sprite", 1, 300.00);
+            tblCart.getItems().add(itemTm);
+        }
+
+        tblCart.refresh();
     }
 
     public void btnSave(ActionEvent actionEvent) {
